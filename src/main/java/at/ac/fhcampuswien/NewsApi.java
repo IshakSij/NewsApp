8d2b197e826de9af95a99cc21c64e78d93707af8
AlimYilmaz <abdulalim.yilmaz@stud.fh-campuswien.ac.at>
2022-05-01 10:29:36 +0200

    NewApi part1

--- src/main/java/at/ac/fhcampuswien/NewsApi.java
+++ src/main/java/at/ac/fhcampuswien/NewsApi.java
@@ -1,10 +1,20 @@
 package at.ac.fhcampuswien;
 
+import com.google.gson.Gson;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import at.ac.fhcampuswien.response.NewsResponse;
+
 public class NewsApi {
 
     private static final String apiKey = "4d43c7f5019b4865b5e5117f1b59943e";
     private static final String url = "https://newsapi.org/v2";
 
+    private static NewsApi instance;
+    private static OkHttpClient client;
+    /*
     private static String endpoint;
     private static String q = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=4d43c7f5019b4865b5e5117f1b59943e";
     private static String category = "https://newsapi.org/v2/everything?category=business&apiKey=4d43c7f5019b4865b5e5117f1b59943e";
@@ -40,4 +50,18 @@ public class NewsApi {
     public static void setSortBy(String sortBy) {
         NewsApi.sortBy = sortBy;
     }
+
+     */
+
+    public static NewsApi getInstance(){
+        if (instance == null){
+            instance = new NewsApi();
+        }
+        return instance;
+    }
+
+    private NewsResponse request(){
+
+    }
+
 }
